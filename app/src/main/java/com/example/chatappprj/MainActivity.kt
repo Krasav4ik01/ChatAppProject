3145178bd773ad08a8da6da49eee9127051080ae
rustem <200103424@stu.sdu.edu.kz>
2023-04-29 18:33:12 +0600

    Замутил Logout

--- app/src/main/java/com/example/chatappprj/MainActivity.kt
+++ app/src/main/java/com/example/chatappprj/MainActivity.kt
@@ -2,18 +2,41 @@ package com.example.chatappprj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var userRecyclerView: RecyclerView
     private lateinit var userList: ArrayList<User>
     private lateinit var adapter: UserAdapter
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        firebaseAuth = FirebaseAuth.getInstance()
 
         userList = ArrayList()
         adapter = UserAdapter(this, userList)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        return super.onOptionsItemSelected(item)
+
+        if (item.itemId == R.id.logout){
+            firebaseAuth.signOut()
+            finish()
+            return true
+        }
+        return true
     }
 }
--- app/src/main/java/com/example/chatappprj/Register.kt
+++ app/src/main/java/com/example/chatappprj/Register.kt
@@ -48,6 +48,7 @@ class Register : AppCompatActivity() {
                     addUserToDatabase(name, email, firebaseAuth.currentUser?.uid!!)
 //                    // Sign in success, update UI with the signed-in user's information
                     val intent = Intent(this@Register, MainActivity::class.java)
+                    finish()
                     startActivity(intent)
                 } else {
                     // If sign in fails, display a message to the user.
